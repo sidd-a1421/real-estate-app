78df0a071d6c3200c5e88ff3049b5810883abd42
Achinta <achintasiddhanta0304@gmail.com>
2025-08-30 18:19:48 +0530

    integrate authentication

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,13 @@
-import { SplashScreen, Stack } from "expo-router";
-import "./global.css";
-import { useFonts } from 'expo-font';
 import { useEffect } from "react";
+import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+
+import "./global.css";
+import GlobalProvider from "@/lib/global-provider";
+
 
 export default function RootLayout() {
-
   const [fontsLoaded] = useFonts({
     "Rubik-Bold": require("../assets/fonts/Rubik-Bold.ttf"),
     "Rubik-ExtraBold": require("../assets/fonts/Rubik-ExtraBold.ttf"),
@@ -15,11 +18,18 @@ export default function RootLayout() {
   });
 
   useEffect(() => {
-    if(fontsLoaded) SplashScreen.hideAsync();
-  },[fontsLoaded])
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
 
-  if(!fontsLoaded) return null;
+  if (!fontsLoaded) {
+    return null;
+  }
 
-  return <Stack screenOptions={{ headerShown: false }} />;
-  
+  return (
+    <GlobalProvider>
+      <Stack screenOptions={{ headerShown: false }} />
+    </GlobalProvider>
+  );
 }
--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,33 +1,71 @@
-import { View, Text, ScrollView, Image, TouchableOpacity } from 'react-native'
-import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import images from '@/constants/images'
-import icons from '@/constants/icons'
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import {
+  Alert,
+  Image,
+  ScrollView,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+
+import { logIn } from "@/lib/appwrite";
+import { Redirect } from "expo-router";
+import { useGlobalContext } from "@/lib/global-provider";
+import icons from "@/constants/icons";
+import images from "@/constants/images";
 
 const SignIn = () => {
+  const { refetch, loading, isLoggedIn } = useGlobalContext();
 
-  const handleLogin = () => {}
+  if (!loading && isLoggedIn) return <Redirect href="/" />;
+
+  const handlelogIn = async () => {
+    const result = await logIn();
+    if (result) {
+      refetch();
+    } else {
+      Alert.alert("Error", "Failed to logIn");
+    }
+  };
 
   return (
-    <SafeAreaView className='bg-white h-full'>
-      <ScrollView contentContainerClassName='h-full' >
-        <Image source={images.onboarding} className='w-full h-4/6' resizeMode='contain' />
-        <View className='px-10'>
-          <Text className='uppercase text-base text-center font-rubik text-black-200'>
-            Welcome to ReState
+    <SafeAreaView className="bg-white h-full">
+      <ScrollView
+        contentContainerStyle={{
+          height: "100%",
+        }}
+      >
+        <Image
+          source={images.onboarding}
+          className="w-full h-4/6"
+          resizeMode="contain"
+        />
+
+        <View className="px-10">
+          <Text className="text-base text-center uppercase font-rubik text-black-200">
+            Welcome To Real Scout
           </Text>
-          <Text className='text-3xl font-rubik-bold text-black-300 text-center mt-1'>
-            Let&apos;s Get You Closer to {'\n'}
-            <Text className='text-primary-300'>
-              Your Ideal Home
-            </Text>
+
+          <Text className="text-3xl font-rubik-bold text-black-300 text-center mt-2">
+            Let&apos;s Get You Closer To {"\n"}
+            <Text className="text-primary-300">Your Ideal Home</Text>
           </Text>
-          <Text className='text-lg font-rubik text-black-200 text-center mt-8'>
-            Login to ReState with Google
+
+          <Text className="text-lg font-rubik text-black-200 text-center mt-3">
+            logIn to Real Scout with Google
           </Text>
-          <TouchableOpacity onPress={handleLogin} className='bg-white shadow-md shadow-zinc-300 rounded-full w-full py-4'>
-            <View className='flex flex-row items-center justify-center mt-2'>
-              <Image source={icons.google} className='w-5 h-5' />
+
+          <TouchableOpacity
+            onPress={handlelogIn}
+            className="bg-white shadow-md shadow-zinc-300 rounded-full w-full py-4 mt-2"
+          >
+            <View className="flex flex-row items-center justify-center">
+              <Image
+                source={icons.google}
+                className="w-5 h-5"
+                resizeMode="contain"
+              />
               <Text className="text-lg font-rubik-medium text-black-300 ml-2">
                 Continue with Google
               </Text>
@@ -36,7 +74,7 @@ const SignIn = () => {
         </View>
       </ScrollView>
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default SignIn
+export default SignIn;
